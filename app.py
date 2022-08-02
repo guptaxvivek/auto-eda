eccb7af72d914473c11471c9a980e29915b637d8
Vivek <gupta.vi2001@gmail.com>
2022-08-02 23:16:37 +0530

    update

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import seaborn as sns
 def get_cat_cols():
     cat_col = []
     for col in data.columns.tolist():
-        if data[col].nunique() < 15:
+        if data[col].nunique() < 10:
             cat_col.append(col)
     return cat_col
 
@@ -18,7 +18,7 @@ with st.sidebar:
         data = pd.read_csv(st.file_uploader(label="Upload CSV file", type='csv'))
     except ValueError:
         pass
-    show = st.selectbox("What to Show", ['DataFrame', "Single column plot", "Multiple column plot"])
+    show = st.selectbox("What to Show", ['DataFrame', 'Column Analysis', "Visualization"])
 
 
 try:
@@ -32,41 +32,71 @@ if show == 'DataFrame':
     st.header("Your DataFrame")
     try:
         st.dataframe(data)
+
+        null_val = data.isna().sum().sum()
+        rows = data.shape[0]
+        null_p = null_val/rows
+        st.subheader(f"NULL Values: {null_val} ({null_p*100:.2f}%)")
+        st.progress(null_p)
+
+        dup_val = data.duplicated().sum().sum()
+        dup_p = dup_val / rows
+        st.subheader(f"Duplicated Rows: {dup_val} ({dup_p*100:.2f}%)")
+        st.progress(dup_p)
+
     except NameError:
         st.write("Please Upload a csv file to proceed..")
 
 
-elif show == "Single column plot":
-    st.header("Single column plot")
-    plot = st.selectbox("Plot Type", ['Countplot', 'PieChart', 'Histogram', 'Boxplot'])
-    if plot == 'Countplot':
-        col = st.selectbox('Select Column', cat_cols)
+elif show == 'Column Analysis':
+    st.header("Column Analysis")
+    sel_col = st.selectbox("Choose column", data.columns.tolist())
+
+    null_val = data[sel_col].isna().sum().sum()
+    null_p = null_val/data[sel_col].shape[0]
+    st.subheader(f"Null Values: {null_val}  ({null_p*100:.2f}%)")
+    st.progress(null_p)
+
+    dup_val = data[sel_col].duplicated().sum()
+    dup_p = dup_val/data[sel_col].shape[0]
+    st.subheader(f"Duplicated Values: {dup_val} ({dup_p*100:.2f}%)")
+    st.progress(dup_p)
+
+    if sel_col in num_cols:
+        st.subheader("Descriptive Stats")
+        st.write(f"Average: {data[sel_col].mean()}")
+        st.write(f"Min: {data[sel_col].min()}")
+        st.write(f"Max: {data[sel_col].max()}")
+
+        st.subheader("Histogram")
         fig = plt.figure(figsize=(10, 4))
-        sns.countplot(data[col])
+        sns.histplot(data[sel_col], kde=True)
+        st.pyplot(fig)
+
+        st.subheader("BoxPlot")
+        fig = plt.figure(figsize=(10, 4))
+        sns.boxplot(data[sel_col])
+        st.pyplot(fig)
+
+    elif sel_col in cat_cols:
+        distinct = data[sel_col].nunique()
+        distinct_p = distinct/data[sel_col].shape[0]
+        st.subheader(f"Distinct Values: {data[sel_col].nunique()} ({distinct_p*100:.2f}%)")
+        st.progress(distinct_p)
+
+        st.subheader("CountPlot")
+        fig = plt.figure(figsize=(10, 4))
+        sns.countplot(data[sel_col])
         plt.xticks(rotation=90)
         st.pyplot(fig)
 
-    elif plot == 'PieChart':
-        col = st.selectbox('Select Column', cat_cols)
+        st.subheader("PieChart")
         fig = plt.figure(figsize=(10, 4))
-        data[col].value_counts().plot(kind='pie', autopct='%.1f')
-        st.pyplot(fig)
-
-    elif plot == 'Histogram':
-        num_col = st.selectbox('Select Column', num_cols)
-        fig = plt.figure(figsize=(10, 4))
-        sns.histplot(data[num_col], kde=True)
-        st.pyplot(fig)
-
-    elif plot == 'Boxplot':
-        num_col = st.selectbox('Select Column', num_cols)
-        fig = plt.figure(figsize=(10, 4))
-        sns.boxplot(data[num_col])
+        data[sel_col].value_counts().plot(kind='pie', autopct='%.1f')
         st.pyplot(fig)
 
 
-
-elif show == "Multiple column plot":
+elif show == "Visualization":
     st.header("Multiple column plot")
 
     type = st.selectbox("Chart Type", ['bar', 'scatter', 'line', 'boxplot', 'regressionplot', 'heatmap', 'pairplot',
@@ -138,5 +168,5 @@ elif show == "Multiple column plot":
         with s3:
             col3 = st.selectbox('Hue Column', cat_cols)
         fig = plt.figure(figsize=(10, 4))
-        sns.scatterplot(x=data[col1], y=data[col2], hue=data[col3], palette='mako', alpha=0.5)
+        sns.scatterplot(x=data[col1], y=data[col2], hue=data[col3], palette='mako',alpha=0.8)
         st.pyplot(fig)
